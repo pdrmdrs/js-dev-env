daa0ea8fb7d2993a73f971a55e7cd7184bbe0cd2
Pedro Medeiros <pdr.mdrs@gmail.com>
2020-04-06 07:52:38 -0300

    feat: add the webpackDevMiddleware on dev server

--- build-scripts/src-server.js
+++ build-scripts/src-server.js
@@ -1,10 +1,20 @@
 import express from "express";
 import path from "path";
 import open from "open";
+import webpack from "webpack";
+import config from "../webpack.config.dev";
+import webpackDevMiddleware from "webpack-dev-middleware";
 
 const port = 3000;
-
 const app = express();
+const compiler = webpack(config);
+
+app.use(
+  webpackDevMiddleware(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath,
+  })
+);
 
 app.get("/", (_, res) => {
   res.sendFile(path.join(__dirname, "../src/index.html"));
