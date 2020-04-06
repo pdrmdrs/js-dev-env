c3e46e5b98bdcf5ccfcc7fff748d62821bb1d171
Pedro Medeiros <pdr.mdrs@gmail.com>
2020-04-06 07:11:30 -0300

    refactor: update js files to use es6 import statements

--- build-scripts/src-server.js
+++ build-scripts/src-server.js
@@ -1,12 +1,12 @@
-const express = require("express");
-const path = require("path");
-const open = require("open");
+import express from "express";
+import path from "path";
+import open from "open";
 
 const port = 3000;
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
   res.sendFile(path.join(__dirname, "../src/index.html"));
 });
 
